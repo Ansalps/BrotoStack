8cd60af2f08ec11cc1afc1d1dd349c3a4deb16de
Ansalps <ansalps10@gmail.com>
2025-09-25 19:16:55 +0530

    changed loading env in main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Ansalps/BrotoStack/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	//err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 	router := gin.Default()
 	routes.RegisterRoutes(router)
 	router.Run(":8080")
