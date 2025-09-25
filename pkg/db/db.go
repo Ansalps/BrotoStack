d0d23823602552c9f9408aea884f2c7bb2f65a81
Ansalps <ansalps10@gmail.com>
2025-09-25 19:41:13 +0530

    changed to render internal dburl credential

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -10,8 +10,9 @@ import (
 )
 
 func ConnectToDb() *gorm.DB{
-	connStr:="user="+os.Getenv("db_user")+" host="+os.Getenv("db_host")+" password="+os.Getenv("db_password")+
-	" dbname="+os.Getenv("db_name")+" port="+os.Getenv("db_port")+" sslmode=disable"
+	// connStr:="user="+os.Getenv("db_user")+" host="+os.Getenv("db_host")+" password="+os.Getenv("db_password")+
+	// " dbname="+os.Getenv("db_name")+" port="+os.Getenv("db_port")+" sslmode=disable"
+	connStr:="postgresql://render:GBLyLQcmMuZAWFDBVNq9z4zAQmLvSJvX@dpg-d3akl6buibrs73ccu3a0-a/my_database_a4hz"
 	db,err:=gorm.Open(postgres.Open(connStr),&gorm.Config{})
 	if err!=nil{
 		log.Fatal(err)
