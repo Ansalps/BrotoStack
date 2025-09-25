e06b469160c420741b418944f5b915392cb1afb3
Ansalps <ansalps10@gmail.com>
2025-09-25 22:27:39 +0530

    using db_url from render as env

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,18 +2,22 @@ package db
 
 import (
 	"log"
+	"os"
 
 	"github.com/Ansalps/BrotoStack/pkg/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func ConnectToDb() *gorm.DB{
-	// connStr:="user="+os.Getenv("db_user")+" host="+os.Getenv("db_host")+" password="+os.Getenv("db_password")+
-	// " dbname="+os.Getenv("db_name")+" port="+os.Getenv("db_port")+" sslmode=disable"
-	connStr:="postgresql://render:GBLyLQcmMuZAWFDBVNq9z4zAQmLvSJvX@dpg-d3akl6buibrs73ccu3a0-a/my_database_a4hz"
-	db,err:=gorm.Open(postgres.Open(connStr),&gorm.Config{})
-	if err!=nil{
+func ConnectToDb() *gorm.DB {
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Println("No .env file found, using environment variables.")
+	// }
+	// connStr := "user=" + os.Getenv("db_user") + " host=" + os.Getenv("db_host") + " password=" + os.Getenv("db_password") +
+	// 	" dbname=" + os.Getenv("db_name") + " port=" + os.Getenv("db_port") + " sslmode=disable"
+	connStr := os.Getenv("db_url")
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
 		log.Fatal(err)
 	}
 	db.AutoMigrate(&models.Users{})
