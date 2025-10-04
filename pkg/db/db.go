299a112954f4a7d82ef09ebf178a6d8c1d0e2b8f
Ansalps <ansalps10@gmail.com>
2025-10-04 19:57:09 +0530

    dropping table users

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,18 +1,20 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/Ansalps/BrotoStack/pkg/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectToDb() *gorm.DB {
-	// if err := godotenv.Load(); err != nil {
-	// 	log.Println("No .env file found, using environment variables.")
-	// }
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables.")
+	}
 	// connStr := "user=" + os.Getenv("db_user") + " host=" + os.Getenv("db_host") + " password=" + os.Getenv("db_password") +
 	// 	" dbname=" + os.Getenv("db_name") + " port=" + os.Getenv("db_port") + " sslmode=disable"
 	connStr := os.Getenv("db_url")
@@ -20,6 +22,13 @@ func ConnectToDb() *gorm.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.AutoMigrate(&models.Users{})
+	err = db.Exec(`drop table users`).Error
+	if err != nil {
+		log.Println("failed to drop tabel users")
+	}
+	err = db.AutoMigrate(&models.Users{}, &models.Admins{}, &models.Otps{})
+	if err != nil {
+		fmt.Println("error in migration", err)
+	}
 	return db
 }
