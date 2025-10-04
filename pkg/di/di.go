38c324b5f6ee78c6e869bf8cb560f785beec6160
Ansalps <ansalps10@gmail.com>
2025-10-04 20:41:00 +0530

    added a print statement in di.go

--- pkg/di/di.go
+++ pkg/di/di.go
@@ -1,20 +1,23 @@
 package di
 
 import (
+	"fmt"
+
 	"github.com/Ansalps/BrotoStack/pkg/db"
 	"github.com/Ansalps/BrotoStack/pkg/handler"
 	"github.com/Ansalps/BrotoStack/pkg/repo"
 	"github.com/Ansalps/BrotoStack/pkg/service"
 )
 
-func DependencyInjection() (*handler.AdminHandler,*handler.UserHandler) {
+func DependencyInjection() (*handler.AdminHandler, *handler.UserHandler) {
+	fmt.Println("Show me some logs")
 	db := db.ConnectToDb()
 	userRepo := repo.NewUserRepo(db)
 	userService := service.NewUserService(userRepo)
 	userHandler := handler.NewUserHandler(userService)
 
-	adminRepo:=repo.NewAdminReop(db)
-	adminService:=service.NewAdminService(adminRepo)
-	adminHandler:=handler.NewAdminHanlder(adminService)
-	return adminHandler,userHandler
+	adminRepo := repo.NewAdminReop(db)
+	adminService := service.NewAdminService(adminRepo)
+	adminHandler := handler.NewAdminHanlder(adminService)
+	return adminHandler, userHandler
 }
